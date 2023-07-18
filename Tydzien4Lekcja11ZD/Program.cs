3e3a3357767930637552a8dd8e2c16c4af4405b6
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-07-18 18:51:16 +0200

    The function of checking the evenness of numbers has been implemented.

--- Tydzien4Lekcja11ZD/Program.cs
+++ Tydzien4Lekcja11ZD/Program.cs
@@ -10,6 +10,52 @@ namespace Tydzien4Lekcja11ZD
 	{
 		static void Main(string[] args)
 		{
+			try
+			{
+				var number = GetInt();
+
+				if (IsEven(number))
+				{
+					Console.Clear();
+					Console.WriteLine($"Liczba {number} jest liczbą parzystą.");
+				}
+				else
+				{
+					Console.Clear();
+					Console.WriteLine($"Liczba {number} jest liczbą nieparzystą.");
+				}
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Wystąpił nieoczekiwany błąd, program zostanie zamknięty!");
+			}
+			finally
+			{
+				Console.Write("Aby zakończyć program naciśnij dowolny klawisz...");
+				Console.ReadKey();
+			}
+		}
+		static int GetInt()
+		{
+			var number = 0;
+
+			Console.Write("Wprowadź liczbę: ");
+
+			while (true)
+			{
+				if (int.TryParse(Console.ReadLine(), out number))
+					break;
+
+				Console.Clear();
+				Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz!");
+				Console.Write("Wprowadź liczbę: ");
+			}
+
+			return number;
+		}
+		static bool IsEven(int number)
+		{
+			return number % 2 == 0;
 		}
 	}
 }
